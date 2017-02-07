f731bebfd8eb8cb1b4266e0210cd9a0efe0302a9
Nick Siemiawski <nicholasrsiemiawsk@lewisu.edu>
2017-02-07 16:34:08 +0000

    new scoring

--- random.rb
+++ random.rb
@@ -1,13 +1,13 @@
 # randomly decides if you passed or failed
 def generator
-    num = Random.rand(2)
-    puts num
-    if num == 0
-        #puts "You failed..."
-        return false
-    elsif num == 1
+    num = Random.rand(100)
+    puts "Score: #{num}%"
+    if num >= 65
         #puts "You passed!!!"
         return true
+    elsif num <= 65
+        #puts "You failed..."
+        return false
     end 
 end 
 
