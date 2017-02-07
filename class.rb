8fb503dcaf1738d62ceaa1fd905d8aabb3b2679f
Nick Siemiawski <nicholasrsiemiawsk@lewisu.edu>
2017-02-07 20:43:35 +0000

    pet class

---
+++ class.rb
@@ -0,0 +1,23 @@
+class Pet
+    attr_accessor :name
+    attr_accessor :type
+    def initialize(name, type)
+        @name = name
+        @type = type
+    end
+    def to_s
+        return "Pet's name is #{name} and is a #{type}"
+    end
+end 
+
+dog = Pet.new("Romeo", "dog")
+puts dog
+cat = Pet.new("Felix", "cat")
+puts cat
+
+#changing dog name attribute
+dog.name = "Kramer"
+dog.type = "good dog"
+puts dog
+        
+    
