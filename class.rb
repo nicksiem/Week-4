be5cf2ab2c2b42ee59a6b7ddadf9770e962013d0
Nick Siemiawski <nicholasrsiemiawsk@lewisu.edu>
2017-02-07 22:37:58 +0000

    added fish

--- class.rb
+++ class.rb
@@ -3,10 +3,12 @@
 class Pet
     attr_accessor :name
     attr_accessor :type
+    
     def initialize(name, type)
         @name = name
         @type = type
     end
+    
     def to_s
         return "Pet's name is #{name} and is a #{type}"
     end
@@ -17,6 +19,8 @@ dog = Pet.new("Romeo", "dog")
 puts dog
 cat = Pet.new("Felix", "cat")
 puts cat
+fish = Pet.new("Bubbles", "fish")
+puts fish
 
 #changing dog name attribute
 dog.name = "Kramer"
--- santa.rb
+++ santa.rb
@@ -13,6 +13,7 @@ entry = 0
 while entry != 3
     print "[1] to add new entry \n[2] to search for niceness by name \n[3] to quit \nEnter option: "
     entry = gets.chomp.to_i
+    
     if entry == 1
         print "Enter name to add: "
         name = gets. chomp
@@ -21,13 +22,16 @@ while entry != 3
         list[name] = n
         puts "The new list:"
         print_list(list)
+        
     elsif entry == 2
         print "Enter name to search for niceness: "
         name = gets.chomp
         puts "#{name} is #{list[name]}"
         puts
+        
     elsif entry == 3
         break
+        
     else
         print "Enter 1 to add, 2 to search, 3 to quit: "
     end
