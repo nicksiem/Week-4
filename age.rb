476a8cb0492378dbc90587d7e0bb7b8769797c8f
Nick Siemiawski <nicholasrsiemiawsk@lewisu.edu>
2017-02-08 16:29:28 +0000

    fixed comments

--- age.rb
+++ age.rb
@@ -1,5 +1,5 @@
 #program prompts user to enter their age
-# prints out 'Are we there yet?' as many times as old they are
+#prints out 'Are we there yet?' as many times as old they are
 
 print "Please enter your age: "
 age = gets.chomp.to_i
